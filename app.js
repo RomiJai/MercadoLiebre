b620edf68681c1b0cdb40adea5e8f4f503a9781c
RomiJai <romina.jaime@icloud.com>
2023-01-21 00:32:41 -0300

    Comenzando el proyecto

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3001;
+
+app.get("/", (req,res) => {
+    res.sendFile(path.resolve(__dirname, "./views/home.html"))
+});
+
+app.listen(PORT, () => {
+    console.log(`server listening on ${PORT}`)
+});

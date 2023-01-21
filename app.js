e0e631300dff29880adf210b92b68e004a73a8e5
RomiJai <romina.jaime@icloud.com>
2023-01-21 01:42:59 -0300

    Agregué logo de ML

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const app = express();
 const PORT = 3001;
 
+app.use(express.static("public"));
+
 app.get("/", (req,res) => {
     res.sendFile(path.resolve(__dirname, "./views/home.html"))
 });

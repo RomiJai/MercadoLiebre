6f67ee6b98f635acaa20975ca0a8ea740ae5c546
RomiJai <romina.jaime@icloud.com>
2023-01-26 18:04:36 -0300

    Modificaciones y ajustes

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
 app.use(express.static("public"));
 
-app.get("/", (req,res) => {
-    res.sendFile(path.resolve(__dirname, "./views/home.html"))
+app.listen(PORT, () => {
+  console.log(`server listening on ${PORT}`);
 });
 
-app.listen(PORT, () => {
-    console.log(`server listening on ${PORT}`)
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/home.html"));
+});
+
+app.get("/views/register.html", (req, res) => {
+  res.sendFile(__dirname + "/views/register.html");
+});
+
+app.get("/views/login.html", (req, res) => {
+  res.sendFile(__dirname + "/views/login.html");
 });
